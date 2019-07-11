d4b2adfd30bfb1e6fe798be04677c457dec8b270
Aaron Lanier <alanier.william@gmail.com>
2019-07-11 14:23:15 -0400

    linked API key

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,15 @@
+// API key = Qghqbph3XryMCV9tmFuZEsm2Y3gRxola
+
+// var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
+
+// xhr.done(function(data) { console.log("success got data", data); });
+
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=Qghqbph3XryMCV9tmFuZEsm2Y3gRxola&limit=25");
+
+xhr.done(function(data) { console.log("success got data", data); });
+
+
+console.log("test")
+
+
+
